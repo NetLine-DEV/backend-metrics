decbf00c80f1e740d913933181aca7ee68d6e69d
gustavbarbosaa <gustavoaraujohab@gmail.com>
2025-05-16 11:21:00 -0300

    recria banco

--- custom_user/migrations/0001_initial.py
+++ custom_user/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth', '0014_remove_user_cpf'),
+        ('auth', '0012_alter_user_first_name_max_length')
     ]
 
     operations = [
